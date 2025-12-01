c5efde8caa4b2d9a7a3301854aea01ada6a60756
Viktor Buiakov <vitkor@softdeveloper.pro>
2025-12-01 22:19:07 +0100

    fix: change mapping

--- src/main/java/com/buyakov/ja/chatop/api/dto/MessageDto.java
+++ src/main/java/com/buyakov/ja/chatop/api/dto/MessageDto.java
@@ -1,5 +1,6 @@
 package com.buyakov.ja.chatop.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
@@ -8,16 +9,8 @@ import lombok.Data;
 public class MessageDto {
     @NotBlank
     private String message;
-    @NotEmpty
-    private Long rental_id;
-    @NotEmpty
-    private Long user_id;
-
-    public Long getRentalId() {
-        return rental_id;
-    }
-
-    public Long getUserId() {
-        return user_id;
-    }
+    @JsonProperty("rental_id")
+    private Long rentalId;
+    @JsonProperty("user_id")
+    private Long userId;
 }
--- src/main/java/com/buyakov/ja/chatop/api/mapper/UserMapper.java
+++ src/main/java/com/buyakov/ja/chatop/api/mapper/UserMapper.java
@@ -3,14 +3,10 @@ package com.buyakov.ja.chatop.api.mapper;
 import com.buyakov.ja.chatop.api.dto.UserInfoResponse;
 import com.buyakov.ja.chatop.api.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 @Component
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    @Mapping(target = "created_at", source = "createdAt")
-    @Mapping(target = "updated_at", source = "updatedAt")
     UserInfoResponse toUserInfoResponse(User user);
 }
